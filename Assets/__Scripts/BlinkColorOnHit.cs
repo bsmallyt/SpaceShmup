3b34f538ae7fa9a1450b4797215bdf211fee30f3
bsmallyt <benjaminsmallyt@gmail.com>
2025-10-31 11:39:23 -0500

    Initial commit

---
+++ Assets/__Scripts/BlinkColorOnHit.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class BlinkColorOnHit : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public float blinkDuration = 0.1f;
+    public Color blinkColor = Color.red;
+
+    [Header("Dynamic")]
+    public bool showingColor = false;
+    public float blinkCompleteTime;
+    public bool ignoreOnCollisionEnter = false;
+
+    private Material[] materials;
+    private Color[] originalColors;
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponentInParent<BoundsCheck>();
+        materials = Utils.GetAllMaterials(gameObject);
+        originalColors = new Color[materials.Length];
+        for (int i = 0; i < materials.Length; i++)
+        {
+            originalColors[i] = materials[i].color;
+        }
+    }
+
+    void Update()
+    {
+        if (showingColor && Time.time > blinkCompleteTime) RevertColors();
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (ignoreOnCollisionEnter) return;
+
+        ProjectileHero p = coll.gameObject.GetComponent<ProjectileHero>();
+        if (p != null)
+        {
+            if (bndCheck != null && !bndCheck.isOnScreen) return;
+            SetColors();
+        }
+    }
+
+    public void SetColors()
+    {
+        foreach (Material m in materials)
+        {
+            m.color = blinkColor;
+        }
+        showingColor = true;
+        blinkCompleteTime = Time.time + blinkDuration;
+    }
+
+    private void RevertColors()
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = originalColors[i];
+        }
+        showingColor = false;
+    }
+}
